c3f5e45a957dfcab71a4952d4e406cb0121261c5
Lê xuan hoang <52564620+hoang11549@users.noreply.github.com>
2021-05-09 17:01:50 +0700

    Bai 10
    get static scss and git

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const express = require('express')
+var morgan = require('morgan')
+var handlebars= require('express-handlebars');
+const app = express()
+const port = 3000;
+app.use(express.static(path.join(__dirname,'public')));
+
+//HTTP logger 
+app.use(morgan('combined'))
+
+//Template engine
+app.engine('hbs', handlebars({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname,'resource/views'))
+
+app.get('/new', (req, res) => {
+  res.render('new');
+})
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
