e27181d7121edc526b6e9cdb25f4b55c1f250a34
Lê xuan hoang <52564620+hoang11549@users.noreply.github.com>
2021-05-10 08:46:57 +0700

    day2
    day2 Ver1

--- src/index.js
+++ src/index.js
@@ -1,28 +1,39 @@
-const path = require('path')
-const express = require('express')
-var morgan = require('morgan')
-var handlebars= require('express-handlebars');
-const app = express()
+const path = require('path');
+const express = require('express');
+var morgan = require('morgan');
+var handlebars = require('express-handlebars');
+const app = express();
 const port = 3000;
-app.use(express.static(path.join(__dirname,'public')));
-
-//HTTP logger 
-app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//HTTP logger
+app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', handlebars({
-  extname:'.hbs'
-}));
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname,'resource/views'))
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
 
 app.get('/new', (req, res) => {
-  res.render('new');
-})
+    res.render('new');
+});
 app.get('/', (req, res) => {
-  res.render('home');
-})
+    res.render('home');
+});
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.send('');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
